eae006df6328573c017e1c2356af5398796ce72c
Roman Yakovlev <yakovlev.roman@gmail.com>
2016-02-11 14:32:22 +0200

    SB-31 Globaldata integration Revert client

--- app/com/globaldata/org/datacontract/schemas/_2004/_07/neuromancerlibrary_xmlserialization_directorukcheck/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.datacontract.org/2004/07/NeuromancerLibrary.XmlSerialization.DirectorUKCheck.Response", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.datacontract.schemas._2004._07.neuromancerlibrary_xmlserialization_directorukcheck;
--- app/com/globaldata/org/datacontract/schemas/_2004/_07/neuromancerlibrary_xmlserialization_idcheckglobalalpha_response/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.datacontract.org/2004/07/NeuromancerLibrary.XmlSerialization.IDCheckGlobalAlpha.Response.CreditReference", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.datacontract.schemas._2004._07.neuromancerlibrary_xmlserialization_idcheckglobalalpha_response;
