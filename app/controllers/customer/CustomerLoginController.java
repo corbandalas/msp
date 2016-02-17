e0551b2dae99fa2af6dbd5530bd5de7252ebfede
corbandalas <me@corbandalas.com>
2016-02-17 13:46:13 +0300

    SB-42. Customer API. Refactoring. Add optionals

--- app/controllers/customer/CustomerLoginController.java
+++ app/controllers/customer/CustomerLoginController.java
@@ -119,7 +119,7 @@ public class CustomerLoginController extends BaseController {
 
             String token = RandomStringUtils.randomAlphanumeric(10);
 
-            cache.set(token,  customer.getId());
+            cache.set(token,customer.getId());
 
             return ok(Json.toJson(new CustomerLoginResponse("0", "Authorization is OK", token, customer.getTemppassword())));
         });
