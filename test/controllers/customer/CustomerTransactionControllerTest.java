485e213975eb254ec1974cbce34113b7ff539d23
ra192 <yakovlev.roman@gmail.com>
2016-03-01 20:20:15 +0200

    SB-54 Money transfer between own cards. Controllers test

--- test/controllers/customer/CustomerTransactionControllerTest.java
+++ test/controllers/customer/CustomerTransactionControllerTest.java
@@ -3,6 +3,7 @@ package controllers.customer;
 import akka.dispatch.Futures;
 import com.fasterxml.jackson.databind.JsonNode;
 import controllers.BaseControllerTest;
+import dto.customer.CustomerCardListResponse;
 import dto.customer.CustomerTransactionFilter;
 import model.Customer;
 import model.enums.KYC;
@@ -26,24 +27,17 @@ import static configs.ReturnCodes.*;
  * @author ra created 17.02.2016.
  * @since 0.1.0
  */
-public class CustomerTransactionControllerTest extends BaseControllerTest {
-
-    private String phone = "4524607605";
+public class CustomerTransactionControllerTest extends BaseCustomerControllerTest {
 
     @Test
     public void listWrongCard() throws Exception {
-        final String password = "hashedpass";
-        final JsonNode createResponse = createCustomer(new Customer(phone, new Date(), "Mr", "Ivan", "Petrenko", "adress1", "adress2",
-                "83004", "Donetsk", "sao@bao.com", new Date(), true, KYC.FULL_DUE_DILIGENCE, password, "USA"));
-        assertEquals("" + SUCCESS_CODE, createResponse.get("code").asText());
-
-        final JsonNode authorizeResponse = authorizeCustomer(phone, password);
+        final JsonNode authorizeResponse = authorizeCustomer(PHONE_1, PASSWORD_1);
         assertEquals("" + SUCCESS_CODE, authorizeResponse.get("code").asText());
 
         final String token = authorizeResponse.get("token").asText();
 
         final CustomerTransactionFilter request = new CustomerTransactionFilter();
-        request.setCardId(1L);
+        request.setCardId(0L);
         request.setFromDate("2016-02-17");
         request.setToDate("2016-02-18");
 
@@ -53,12 +47,26 @@ public class CustomerTransactionControllerTest extends BaseControllerTest {
         assertEquals("" + INCORRECT_CARD_CODE,changeResponse.get("code").asText());
     }
 
-    @After
-    public void clean() throws Exception {
-        final ConnectionPool connectionPool = app.injector().instanceOf(ConnectionPool.class);
-        final Promise<Object> promise = Futures.promise();
-        connectionPool.getConnection().query("delete from " + connectionPool.getSchemaName() + ".customer where id=$1", asList(phone),
-                promise::success, promise::failure);
-        Await.result(promise.future(), Duration.apply(TIMEOUT,"ms"));
+    @Test
+    public void list() throws Exception {
+        final JsonNode authorizeResponse = authorizeCustomer(PHONE_1, PASSWORD_1);
+        assertEquals("" + SUCCESS_CODE, authorizeResponse.get("code").asText());
+
+        final String token = authorizeResponse.get("token").asText();
+
+        final JsonNode cardListResponse = WS.url(getCustomerApiUrl("/card/list")).setHeader("token", token)
+                .get().get(TIMEOUT).asJson();
+        assertEquals("" + SUCCESS_CODE, cardListResponse.get("code").asText());
+        final CustomerCardListResponse customerCardListResponse = Json.fromJson(cardListResponse, CustomerCardListResponse.class);
+
+        final CustomerTransactionFilter request = new CustomerTransactionFilter();
+        request.setCardId(customerCardListResponse.getList().get(0).getId());
+        request.setFromDate("2016-02-17");
+        request.setToDate("2016-02-18");
+
+        final JsonNode changeResponse = WS.url(getCustomerApiUrl("/transaction/list")).setHeader("token", token)
+                .post(Json.toJson(request)).get(TIMEOUT).asJson();
+
+        assertEquals("" + INCORRECT_CARD_CODE,changeResponse.get("code").asText());
     }
 }
--- test/controllers/customer/CustomerTransferControllerTest.java
+++ test/controllers/customer/CustomerTransferControllerTest.java
@@ -1,25 +1,15 @@
 package controllers.customer;
 
-import akka.dispatch.Futures;
 import com.fasterxml.jackson.databind.JsonNode;
-import controllers.BaseControllerTest;
+import dto.customer.CustomerCardListResponse;
 import dto.customer.CustomerTransferOwnCards;
-import model.Customer;
-import model.enums.KYC;
-import org.junit.After;
 import org.junit.Test;
 import play.libs.Json;
 import play.libs.ws.WS;
-import repository.ConnectionPool;
-import scala.concurrent.Await;
-import scala.concurrent.Promise;
-import scala.concurrent.duration.Duration;
 
-import java.util.Date;
-
-import static java.util.Arrays.asList;
+import static configs.ReturnCodes.INCORRECT_CARD_CODE;
+import static configs.ReturnCodes.SUCCESS_CODE;
 import static org.junit.Assert.assertEquals;
-import static configs.ReturnCodes.*;
 
 /**
  * API customer transfer controller tests
@@ -28,16 +18,9 @@ import static configs.ReturnCodes.*;
  */
 public class CustomerTransferControllerTest extends BaseCustomerControllerTest {
 
-    private String phone = "4524607605";
-
     @Test
     public void transferOwnWrongCard() throws Exception {
-        final String password = "hashedpass";
-        final JsonNode createResponse = createCustomer(new Customer(phone, new Date(), "Mr", "Ivan", "Petrenko", "adress1", "adress2",
-                "83004", "Donetsk", "sao@bao.com", new Date(), true, KYC.FULL_DUE_DILIGENCE, password, "USA"));
-        assertEquals("" + SUCCESS_CODE, createResponse.get("code").asText());
-
-        final JsonNode authorizeResponse = authorizeCustomer(phone, password);
+        final JsonNode authorizeResponse = authorizeCustomer(PHONE_1, PASSWORD_1);
         assertEquals("" + SUCCESS_CODE, authorizeResponse.get("code").asText());
 
         final String token = authorizeResponse.get("token").asText();
@@ -45,23 +28,39 @@ public class CustomerTransferControllerTest extends BaseCustomerControllerTest {
         final CustomerTransferOwnCards request = new CustomerTransferOwnCards();
         request.setAmount(1000L);
         request.setCurrency("USD");
-        request.setCardFrom(1L);
-        request.setCardTo(2L);
+        request.setCardFrom(0L);
+        request.setCardTo(1L);
         request.setDescription("test transfer");
         request.setOrderId("xxxx");
 
-        final JsonNode changeResponse = WS.url(getCustomerApiUrl("/transfer/own")).setHeader("token", token)
+        final JsonNode transferResponse = WS.url(getCustomerApiUrl("/transfer/own")).setHeader("token", token)
                 .post(Json.toJson(request)).get(TIMEOUT).asJson();
 
-        assertEquals("" + INCORRECT_CARD_CODE,changeResponse.get("code").asText());
+        assertEquals("" + INCORRECT_CARD_CODE,transferResponse.get("code").asText());
     }
 
-    @After
-    public void clean() throws Exception {
-        final ConnectionPool connectionPool = app.injector().instanceOf(ConnectionPool.class);
-        final Promise<Object> promise = Futures.promise();
-        connectionPool.getConnection().query("delete from " + connectionPool.getSchemaName() + ".customer where id=$1", asList(phone),
-                promise::success, promise::failure);
-        Await.result(promise.future(), Duration.apply(TIMEOUT,"ms"));
+    @Test
+    public void transferOwn() throws Exception {
+        final JsonNode authorizeResponse = authorizeCustomer(PHONE_1, PASSWORD_1);
+        assertEquals("" + SUCCESS_CODE, authorizeResponse.get("code").asText());
+
+        final String token = authorizeResponse.get("token").asText();
+
+        final JsonNode cardListResponse = WS.url(getCustomerApiUrl("/card/list")).setHeader("token", token)
+                .get().get(TIMEOUT).asJson();
+        assertEquals("" + SUCCESS_CODE, cardListResponse.get("code").asText());
+        final CustomerCardListResponse customerCardListResponse = Json.fromJson(cardListResponse, CustomerCardListResponse.class);
+
+        final CustomerTransferOwnCards request = new CustomerTransferOwnCards();
+        request.setAmount(100L);
+        request.setCurrency("USD");
+        request.setCardFrom(customerCardListResponse.getList().get(0).getId());
+        request.setCardTo(customerCardListResponse.getList().get(1).getId());
+        request.setDescription("test transfer");
+        request.setOrderId("xxxx");
+
+        final JsonNode transferResponse = WS.url(getCustomerApiUrl("/transfer/own")).setHeader("token", token)
+                .post(Json.toJson(request)).get(TIMEOUT).asJson();
+        assertEquals(""+SUCCESS_CODE,transferResponse.get("code").asText());
     }
 }
