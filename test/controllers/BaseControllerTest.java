13ef77d59ec186dc0d16937f3cf0d6df1931a728
Roman Yakovlev <yakovlev.roman@gmail.com>
2016-05-30 12:07:02 +0300

    SB-74 Integration with WorldPay hosted payment page(credit card)

--- test/controllers/BaseControllerTest.java
+++ test/controllers/BaseControllerTest.java
@@ -12,20 +12,17 @@ import model.Customer;
 import model.Operation;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.internal.runners.statements.Fail;
 import play.libs.Json;
 import play.libs.ws.WS;
 import play.test.WithServer;
 import repository.ConnectionPool;
 import scala.concurrent.Await;
-import scala.concurrent.Future;
 import scala.concurrent.Promise;
 import scala.concurrent.duration.Duration;
 import util.SecurityUtil;
 
 import java.sql.Timestamp;
 import java.util.Date;
-import java.util.logging.Logger;
 
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
@@ -42,6 +39,7 @@ public class BaseControllerTest extends WithServer {
     public final String ORDER_ID = "1";
     public final String ACCOUNT_ID = "42";
     public final String ACCOUNT_2_ID = "43";
+    public final String ACCOUNT_3_ID = "44";
     public final String SECRET = "OMG";
     public final String ACCOUNT_NAME = "God account";
     public final long TIMEOUT = 10000;
@@ -54,29 +52,20 @@ public class BaseControllerTest extends WithServer {
         connectionPool = app.injector().instanceOf(ConnectionPool.class);
         connectionPool.setSchemaName(conf.getString("database.test.schema"));
         try {
-            insertAccount();
-            insertAccount2();
+            insertAccount(ACCOUNT_ID);
+            insertAccount(ACCOUNT_2_ID);
+            insertAccount(ACCOUNT_3_ID);
         } catch (Exception e) {
             e.printStackTrace();
             fail();
         }
     }
 
-    private void insertAccount() throws Exception {
+    private void insertAccount(String account_id) throws Exception {
         final ConnectionPool connectionPool = app.injector().instanceOf(ConnectionPool.class);
         final Promise<Object> promise = Futures.promise();
         connectionPool.getConnection().query("INSERT INTO " + connectionPool.getSchemaName() + ".account(id, currency_id, name," +
-                        " createdate, active, secret) VALUES ($1, $2, $3, $4, $5, $6)", asList(ACCOUNT_ID, "USD", "God account",
-                new Timestamp(new Date().getTime()), true, SECRET),
-                promise::success, promise::failure);
-        Await.result(promise.future(), Duration.apply(TIMEOUT, "ms"));
-    }
-
-    private void insertAccount2() throws Exception {
-        final ConnectionPool connectionPool = app.injector().instanceOf(ConnectionPool.class);
-        final Promise<Object> promise = Futures.promise();
-        connectionPool.getConnection().query("INSERT INTO " + connectionPool.getSchemaName() + ".account(id, currency_id, name," +
-                        " createdate, active, secret) VALUES ($1, $2, $3, $4, $5, $6)", asList(ACCOUNT_2_ID, "USD", "God account",
+                        " createdate, active, secret) VALUES ($1, $2, $3, $4, $5, $6)", asList(account_id, "USD", "God account",
                 new Timestamp(new Date().getTime()), true, SECRET),
                 promise::success, promise::failure);
         Await.result(promise.future(), Duration.apply(TIMEOUT, "ms"));
@@ -100,7 +89,7 @@ public class BaseControllerTest extends WithServer {
 
         final ConnectionPool connectionPool = app.injector().instanceOf(ConnectionPool.class);
         final Promise<Object> promise = Futures.promise();
-        connectionPool.getConnection().query("delete from " + connectionPool.getSchemaName() + ".account where id=$1 or id=$2", asList(ACCOUNT_ID, ACCOUNT_2_ID),
+        connectionPool.getConnection().query("delete from " + connectionPool.getSchemaName() + ".account where id=$1 or id=$2 or id=$3", asList(ACCOUNT_ID, ACCOUNT_2_ID, ACCOUNT_3_ID),
                 promise::success, promise::failure);
         Await.result(promise.future(), Duration.apply(TIMEOUT, "ms"));
     }
