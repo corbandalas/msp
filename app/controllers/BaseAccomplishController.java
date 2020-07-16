139b6bf8dddcb3909796f27c7bb9927e00c5970f
corbandalas <me@corbandalas.com>
2020-07-16 10:44:31 +0300

    Initial commit of MSP

--- app/controllers/BaseAccomplishController.java
+++ app/controllers/BaseAccomplishController.java
@@ -24,82 +24,82 @@ import static configs.ReturnCodesV2.*;
 
 public class BaseAccomplishController extends play.mvc.Controller {
 
-    protected Status createKycLimitResponse() {
+    protected Results.Status createKycLimitResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(KYC_RESTRICTION_TEXT, "" + KYC_RESTRICTION_CODE, "", "")));
     }
 
 
-    protected Status createIncorrectCurrencyResponse() {
+    protected Results.Status createIncorrectCurrencyResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(INCORRECT_CURRENCY_TEXT, "" + INCORRECT_CURRENCY_CODE, "", "")));
     }
 
-    protected Status createIncorrectCountryResponse() {
+    protected Results.Status createIncorrectCountryResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(INCORRECT_COUNTRY_TEXT, "" + INCORRECT_COUNTRY_CODE, "", "")));
     }
 
-    protected Status createAlreadyRegisteredCustomerAccountResponse() {
+    protected Results.Status createAlreadyRegisteredCustomerAccountResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(ALREADY_REGISTERED_CUSTOMER_ACCOUNT_TEXT, "" + ALREADY_REGISTERED_CUSTOMER_ACCOUNT_CODE, "", "")));
     }
 
-    protected Status createWrongCardResponse() {
+    protected Results.Status createWrongCardResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(INCORRECT_CARD_TEXT, "" + INCORRECT_CARD_CODE, "", "")));
     }
 
-    protected Status createCurrencyNotTheSameResponse() {
+    protected Results.Status createCurrencyNotTheSameResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(CURRENCIES_NOT_EQUAL_TEXT, "" + CURRENCIES_NOT_EQUAL_CODE, "", "")));
     }
 
-    protected Status createWrongTransferKYCResponse() {
+    protected Results.Status createWrongTransferKYCResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(KYC_TRANSFER_RESTRICTION_TEXT, "" + KYC_TRANSFER_RESTRICTION_CODE, "", "")));
     }
 
-    protected Status createNotEnoughFundsResponse() {
+    protected Results.Status createNotEnoughFundsResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(NOT_ENOUGH_FUNDS_TEXT, "" + NOT_ENOUGH_FUNDS_CODE, "", "")));
     }
 
-    protected Status createCardProviderException(String errorCode) {
+    protected Results.Status createCardProviderException(String errorCode) {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(CARD_PROVIDER_EXCEPTION_TEXT, "" + CARD_PROVIDER_EXCEPTION_CODE, errorCode, "")));
     }
 
-    protected Status createWrongKYCResponse() {
+    protected Results.Status createWrongKYCResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(INCORRECT_KYC_TEXT, "" + INCORRECT_KYC_CODE, "", "")));
     }
 
-    protected Status createLimitsExceededResponse() {
+    protected Results.Status createLimitsExceededResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(LIMITS_EXCEEDED_TEXT, "" + LIMITS_EXCEEDED_CODE, "", "")));
     }
 
-    protected Status createGeneralErrorResponse() {
+    protected Results.Status createGeneralErrorResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(GENERAL_ERROR_TEXT, "" + GENERAL_ERROR_CODE, "", "")));
     }
 
-    protected Status createWrongCustomerAccountResponse() {
+    protected Results.Status createWrongCustomerAccountResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(WRONG_CUSTOMER_ACCOUNT_TEXT, "" + WRONG_CUSTOMER_ACCOUNT_CODE, "", "")));
     }
 
-    protected Status createPasswordMismatchResponse() {
+    protected Results.Status createPasswordMismatchResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(PASSWORD_MISMATCH_TEXT, "" + PASSWORD_MISMATCH_CODE, "", "")));
     }
 
-    protected Status createPasswordEqualsToExistedResponse() {
+    protected Results.Status createPasswordEqualsToExistedResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(PASSWORD_EQUALS_TO_EXISTED_TEXT, "" + PASSWORD_EQUALS_TO_EXISTED_CODE, "", "")));
     }
 
 
-    protected Status createWrongRequestFormatResponse() {
+    protected Results.Status createWrongRequestFormatResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(WRONG_REQUEST_FORMAT_TEXT, "" + WRONG_REQUEST_FORMAT_CODE, "", "")));
     }
 
-    protected Status createWrongRequestFormatResponse(String details) {
+    protected Results.Status createWrongRequestFormatResponse(String details) {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(WRONG_REQUEST_FORMAT_TEXT, "" + WRONG_REQUEST_FORMAT_CODE, details, "")));
     }
 
 
-    protected Status createWrongPhoneNumberResponse() {
+    protected Results.Status createWrongPhoneNumberResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(INCORRECT_PHONE_NUMBER_TEXT, "" + INCORRECT_PHONE_NUMBER_CODE, "", "")));
     }
 
-    protected Status createPasswordExceededResponse() {
+    protected Results.Status createPasswordExceededResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(PASSWORD_ATTEMPTS_EXCEEDED_TEXT, "" + PASSWORD_ATTEMPTS_EXCEEDED_CODE, "", "")));
     }
 
@@ -107,7 +107,7 @@ public class BaseAccomplishController extends play.mvc.Controller {
         return redirect(url);
     }
 
-    protected Status createWrongEncKeyResponse() {
+    protected Results.Status createWrongEncKeyResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(WRONG_REQUEST_ENCKEY_TEXT, "" + WRONG_REQUEST_ENCKEY_CODE, "", "")));
     }
 
@@ -124,7 +124,7 @@ public class BaseAccomplishController extends play.mvc.Controller {
         return new PropertyResponse(text, code, property);
     }
 
-    protected Status createIncorrectAccountResponse() {
+    protected Results.Status createIncorrectAccountResponse() {
         return badRequest(Json.toJson(new BaseAPIV2ErrorResponse(INCORRECT_ACCOUNT_TEXT, "" + INCORRECT_ACCOUNT_CODE, "", "")));
     }
 
