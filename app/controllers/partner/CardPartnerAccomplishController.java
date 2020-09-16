39e07db6c869f5aa0d79876763e1bfc41cd56142
corbandalas <me@corbandalas.com>
2020-09-16 16:57:50 +0300

    Initial commit of MSP

--- app/controllers/partner/CardPartnerAccomplishController.java
+++ app/controllers/partner/CardPartnerAccomplishController.java
@@ -2010,7 +2010,9 @@ public class CardPartnerAccomplishController extends BaseAccomplishController {
 
             Boolean aBoolean1 = F.Promise.wrap(cardRepository.deleteAllCards(res.get().getId())).get(10000);
             Boolean aBoolean = F.Promise.wrap(customerRepository.deleteCustomer(res.get().getId())).get(10000);
-            accomplishService.updateUserPhone(res.get().getReferral(), res.get().getPhone2() + "0", "" + authData.getAccount().getId()).get(10000);
+                    accomplishService.updateUserEmail(res.get().getReferral(), res.get().getPhone2() + "0@me.com", "" + authData.getAccount().getId()).get(10000);
+
+                    accomplishService.updateUserPhone(res.get().getReferral(), res.get().getPhone2() + "0", "" + authData.getAccount().getId()).get(10000);
 
 
             return ok(Json.toJson(new SuccessAPIV2Response(true)));
