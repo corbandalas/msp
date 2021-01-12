2c0b4adde24e08b75d7c362255f134ebad65fb8a
corbandalas <me@corbandalas.com>
2021-01-12 15:51:57 +0300

    Initial commit of MSP

--- app/controllers/partner/CardPartnerAccomplishController.java
+++ app/controllers/partner/CardPartnerAccomplishController.java
@@ -1014,7 +1014,7 @@ public class CardPartnerAccomplishController extends BaseAccomplishController {
 
                     String type = "1";
 
-                    if (data._2.get().getBrand().equals(CardBrand.CHILDCARD)) {
+                    if (data._2.get().getType().equals("physical")) {
                         type = "0";
                     } else {
                         return F.Promise.pure(ok(Json.toJson(new SuccessAPIV2Response(true))));
