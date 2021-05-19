1cebfdbb02a1557a772afe728874ce9eb3a7a856
corbandalas <me@corbandalas.com>
2021-05-19 15:50:03 +0300

    add coinify card program

--- app/controllers/partner/CardPartnerAccomplishController.java
+++ app/controllers/partner/CardPartnerAccomplishController.java
@@ -984,6 +984,16 @@ public class CardPartnerAccomplishController extends BaseAccomplishController {
                                         currency = "EUR";
                                         cardType = CardType.PLASTIC;
                                         type = "physical";
+                                    } else if (createCard.getCardModel().equalsIgnoreCase("coinify_eur")) {
+                                        cardBrand = CardBrand.VISA;
+                                        currency = "EUR";
+                                        cardType = CardType.PLASTIC;
+                                        type = "physical";
+                                    } else if (createCard.getCardModel().equalsIgnoreCase("coinify_gbp")) {
+                                        cardBrand = CardBrand.VISA;
+                                        currency = "GBP";
+                                        cardType = CardType.PLASTIC;
+                                        type = "physical";
                                     }
 
 
