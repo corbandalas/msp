8be11e99163683f930d436679c0076939c92098e
corbandalas <me@corbandalas.com>
2021-05-27 11:33:06 +0300

    Initial commit of MSP

--- app/dto/partnerV2/CheckCardProviderPasswordRequest.java
+++ app/dto/partnerV2/CheckCardProviderPasswordRequest.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CheckCardProviderPasswordRequest {
 
-    private String mobilePhone;
+    private String email;
     private String password;
 
 
